7a61009b65ce63cd313db6aedb9ccacba3f82374
Uahhhhhh <hnyrarara@gmail.com>
2021-10-28 06:45:21 +0800

    Accurate IP to city.

--- app.py
+++ app.py
@@ -21,9 +21,21 @@ if __name__ == '__main__':
     @app.route('/', methods=["GET", "POST"])
     @app.route('/index', methods=["GET", "POST"])
     def index():
-        # city = temp.from_ip_to_city(request.remote_addr)
-        city, _ = IP.from_ip_to_city('59.49.184.209')
-        if '省' in city or '市' in city or '县' in city or '地区' in city:
+        # city, _ = IP.from_ip_to_city(request.remote_addr)
+        city, _ = IP.from_ip_to_city('120.204.100.39')
+        if '新疆' in city or '西藏' in city or '宁夏' in city or '内蒙' in city or '广西' in city and '市' in city:
+            # print(city)
+            if '新疆' in city:
+                city = city[0:city.find('新疆') + 2] + '省' + city[city.find('新疆') + 2:]
+            if '西藏' in city:
+                city = city[0:city.find('西藏') + 2] + '省' + city[city.find('西藏') + 2:]
+            if '宁夏' in city:
+                city = city[0:city.find('宁夏') + 2] + '省' + city[city.find('宁夏') + 2:]
+            if '内蒙' in city:
+                city = city[0:city.find('内蒙') + 3] + '省' + city[city.find('内蒙') + 3:]
+            if '广西' in city:
+                city = city[0:city.find('广西') + 2] + '省' + city[city.find('广西') + 2:]
+        if '省' in city or '市' in city or '县' in city or '区' in city:
             if '省' in city and '市' in city:
                 begin = city.find('省') + 1
                 end = city.find('市') + 1
@@ -31,6 +43,10 @@ if __name__ == '__main__':
             elif '省' in city and '县' in city:
                 begin = city.find('省') + 1
                 end = city.find('县') + 1
+                city = city[begin:end]
+            elif '市' in city and '区' in city:
+                begin = 0
+                end = city.find('市') + 1
                 city = city[begin:end]
         return render_template('board.html', result_json=json.dumps({'a': IP.city_gps[city]}))
 
