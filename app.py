d65a8fc431f93bd38318b521f02c6c063d68a5fb
Uahhhhhh <hnyrarara@gmail.com>
2021-11-17 05:16:34 +0800

    好累啊。

--- app.py
+++ app.py
@@ -1,83 +1,143 @@
 # -*- coding: utf-8 -*-
+import time
 import json
 import config
+import sqlite3
 from models import functions
 from models.model import IP
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask_apscheduler import APScheduler
 
 
-def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
-    app.config.from_object(config)
-    return app
+def update_city_json():
+    conn = sqlite3.connect(r'data/comic_database.db')
+    c = conn.cursor()
+    cursor = c.execute("SELECT \"index\", value, city, province, longitude, latitude FROM city")
+    ans = {
+        "cities": []
+    }
+    for raw in cursor:
+        temp = [raw[0], raw[1], raw[2], raw[3], raw[4], raw[5]]
+        ans['cities'].append(temp)
+
+    with open("data/city_table.json", "w", encoding='utf-8') as f:
+        json.dump(ans, f, ensure_ascii=False, sort_keys=True, indent=4)
+    print(time.asctime(time.localtime(time.time())))
+    print("Json文件已更新")
+
+
+def update_province_json():
+    conn = sqlite3.connect(r'data/comic_database.db')
+    c = conn.cursor()
+    cursor = c.execute("SELECT \"index\", value, city, province, longitude, latitude FROM city")
+    temp = {}
+    for raw in cursor:
+        if raw[3] in temp.keys():
+            temp[raw[3]] += raw[1]
+        else:
+            temp[raw[3]] = 0
+    res = []
+    for i in temp.keys():
+        res.append(
+            {
+                'name': i,
+                'value': temp[i]
+            }
+        )
+    with open("data/province_table.json", "w", encoding='utf-8') as f:
+        json.dump(res, f, ensure_ascii=False, sort_keys=True, indent=4)
+    print(time.asctime(time.localtime(time.time())))
+    print("Json文件已更新")
+
+
+class SchedulerConfig(object):
+    JOBS = [
+        {
+            'id': 'update_city_json',  # 任务id
+            'func': '__main__:update_city_json',  # 任务执行程序
+            'args': None,  # 执行程序参数
+            'trigger': 'interval',  # 任务执行类型，定时器
+            'seconds': 20,  # 任务执行时间，单位秒
+        },
+        {
+            'id': 'update_province_json',  # 任务id
+            'func': '__main__:update_province_json',  # 任务执行程序
+            'args': None,  # 执行程序参数
+            'trigger': 'interval',  # 任务执行类型，定时器
+            'seconds': 21,  # 任务执行时间，单位秒
+        }
+    ]
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config.from_object(config)
+app.config.from_object(SchedulerConfig)
+print('Waiting......')
+
+
+# 主要逻辑视图函数
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    city, _ = IP.from_ip_to_city('120.204.100.39')
+    if '新疆' in city or '西藏' in city or '宁夏' in city or '内蒙' in city or '广西' in city and '市' in city:
+        # print(city)
+        if '新疆' in city:
+            city = city[0:city.find('新疆') + 2] + '省' + city[city.find('新疆') + 2:]
+        if '西藏' in city:
+            city = city[0:city.find('西藏') + 2] + '省' + city[city.find('西藏') + 2:]
+        if '宁夏' in city:
+            city = city[0:city.find('宁夏') + 2] + '省' + city[city.find('宁夏') + 2:]
+        if '内蒙' in city:
+            city = city[0:city.find('内蒙') + 3] + '省' + city[city.find('内蒙') + 3:]
+        if '广西' in city:
+            city = city[0:city.find('广西') + 2] + '省' + city[city.find('广西') + 2:]
+    if '省' in city or '市' in city or '县' in city or '区' in city:
+        if '省' in city and '市' in city:
+            begin = city.find('省') + 1
+            end = city.find('市') + 1
+            city = city[begin:end]
+        elif '省' in city and '县' in city:
+            begin = city.find('省') + 1
+            end = city.find('县') + 1
+            city = city[begin:end]
+        elif '市' in city and '区' in city:
+            begin = 0
+            end = city.find('市') + 1
+            city = city[begin:end]
+    return render_template('index.html', result_json=json.dumps({'a': city}))
+
+
+@app.route('/2', methods=["GET", "POST"])
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/get_data', methods=["GET", "POST"])
+def get_data():
+    with open(r'data/province_table.json', encoding='utf-8') as f:
+        data = json.load(f)
+        f.close()
+    return json.dumps(data)
+
+
+@app.route('/geo_china_json', methods=["GET", "POST"])
+def geo_china_json():
+    with open(r'data/chinageo.json', encoding='utf-8') as f:
+        test_line = json.load(f)
+        f.close()
+    return test_line
+
+
+@app.route('/error')
+def error():
+    return '404 not found'
 
 
 if __name__ == '__main__':
-    app = create_app()
-    print('Waiting......')
-
-    # 主要逻辑视图函数
-    @app.route('/', methods=["GET", "POST"])
-    @app.route('/index', methods=["GET", "POST"])
-    def index():
-        # city, _ = IP.from_ip_to_city(request.remote_addr)
-        city, _ = IP.from_ip_to_city('120.204.100.39')
-        if '新疆' in city or '西藏' in city or '宁夏' in city or '内蒙' in city or '广西' in city and '市' in city:
-            # print(city)
-            if '新疆' in city:
-                city = city[0:city.find('新疆') + 2] + '省' + city[city.find('新疆') + 2:]
-            if '西藏' in city:
-                city = city[0:city.find('西藏') + 2] + '省' + city[city.find('西藏') + 2:]
-            if '宁夏' in city:
-                city = city[0:city.find('宁夏') + 2] + '省' + city[city.find('宁夏') + 2:]
-            if '内蒙' in city:
-                city = city[0:city.find('内蒙') + 3] + '省' + city[city.find('内蒙') + 3:]
-            if '广西' in city:
-                city = city[0:city.find('广西') + 2] + '省' + city[city.find('广西') + 2:]
-        if '省' in city or '市' in city or '县' in city or '区' in city:
-            if '省' in city and '市' in city:
-                begin = city.find('省') + 1
-                end = city.find('市') + 1
-                city = city[begin:end]
-            elif '省' in city and '县' in city:
-                begin = city.find('省') + 1
-                end = city.find('县') + 1
-                city = city[begin:end]
-            elif '市' in city and '区' in city:
-                begin = 0
-                end = city.find('市') + 1
-                city = city[begin:end]
-        return render_template('index.html', result_json=json.dumps({'a': city}))
-
-    @app.route('/2', methods=["GET", "POST"])
-    def index2():
-        return render_template('index2.html')
-
-    @app.route('/get_point_json', methods=["GET", "POST"])
-    def get_json():
-        with open(r'data/test_point.json', encoding='utf-8') as f:
-            test_gps = json.load(f)
-            f.close()
-        return test_gps
-
-    @app.route('/get_line_json', methods=["GET", "POST"])
-    def get_line_json():
-        with open(r'data/city_line2.json', encoding='utf-8') as f:
-            test_line = json.load(f)
-            f.close()
-        return test_line
-
-    @app.route('/geo_china_json', methods=["GET", "POST"])
-    def geo_china_json():
-        with open(r'data/chinageo.json', encoding='utf-8') as f:
-            test_line = json.load(f)
-            f.close()
-        return test_line
-
-    @app.route('/error/')
-    def error():
-        return '404 not found'
-
-    app.run(host='0.0.0.0', debug=True, port=173)
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run(host='0.0.0.0', debug=False, port=173)
--- models/creat_database.py
+++ models/creat_database.py
@@ -6,23 +6,44 @@ conn = sqlite3.connect(r'../data/comic_database.db')
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE city_points (
-             [index]   INTEGER PRIMARY KEY,
-             city      STRING  NOT NULL,
-             longitude DOUBLE  NOT NULL,
-             latitude  DOUBLE  NOT NULL
-         );''')
+# c.execute('''CREATE TABLE city_points (
+#              [index]   INTEGER PRIMARY KEY,
+#              value     INTEGER NOT NULL,
+#              city      STRING  NOT NULL,
+#              province  STRING  NOT NULL,
+#              longitude DOUBLE  NOT NULL,
+#              latitude  DOUBLE  NOT NULL
+#          );''')
+#
 
-conn.commit()
+# c.execute('''CREATE TABLE province (
+#     name  STRING  PRIMARY KEY,
+#     value INTEGER NOT NULL
+# );
+# ''')
+# conn.commit()
 
-with open(r'../data/city_point_test_table.json', encoding='utf-8') as json_file:
+# with open(r'../data/city_table.json', encoding='utf-8') as json_file:
+#     table = json.load(json_file)
+#     json_file.close()
+#
+# for i in range(len(table['cities'])):
+#     # print("INSERT INTO city (\"index\", value, city, province, longitude, latitude) \
+#     #       VALUES (" + str(i) + ", 0, \"" + table['cities'][i][0] + "\", \"" + str(table['cities'][i][5]) + "\", " +
+#     #           str(table['cities'][i][1]) + ", " + str(table['cities'][i][2]) + ")")
+#     c.execute("INSERT INTO city (\"index\", value, city, province, longitude, latitude) \
+#           VALUES (" + str(i) + ", 0, \"" + table['cities'][i][0] + "\", \"" + str(table['cities'][i][5]) + "\", " +
+#               str(table['cities'][i][1]) + ", " + str(table['cities'][i][2]) + ")")
+
+with open(r'../data/province_table.json', encoding='utf-8') as json_file:
     table = json.load(json_file)
     json_file.close()
-# pprint(table)
-for i in range(len(table['cities'])):
-    c.execute("INSERT INTO city_points (\"index\",city,longitude,latitude) \
-          VALUES (" + str(i) + ", \"" + table['cities'][i][0] + "\", "
-              + str(table['cities'][i][1]) + ", " + str(table['cities'][i][2]) + ")")
+
+for i in range(len(table['provinces'])):
+    print("INSERT INTO province (name, value) \
+          VALUES (\"" + table['provinces'][i]['name'] + "\", 0)")
+    c.execute("INSERT INTO province (name, value) \
+              VALUES (\"" + table['provinces'][i]['name'] + "\", 0)")
 
 conn.commit()
 conn.close()
